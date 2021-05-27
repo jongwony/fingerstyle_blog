c97beab81977dec57157a47b7b768930b7ea7553
jongwony <lastone9182@gmail.com>
2021-05-28 01:31:48 +0900

    git to https

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jongwony/fingerstyle.git master:gh-pages
+git push -f https://github.com/jongwony/fingerstyle.git master:gh-pages
 
 cd -
